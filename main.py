7cc3dfa8bd8160bf2cff8473bb6d15afc1a62a7c
Daksh Solanki <solankidaksh97@gmail.com>
2025-11-29 14:55:00 +0530

    Fixed CORS origin (removed trailing slash)

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app = FastAPI()
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins = ["https://frontend-tau-tawny-72.vercel.app",
+    "http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
